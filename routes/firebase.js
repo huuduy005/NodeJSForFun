e53dee588b922b923a3ffea905cc44fdc9a77133
huuduy005 <duy.doan@vexere.com>
2018-01-26 11:07:43 +0700

    update

--- routes/firebase.js
+++ routes/firebase.js
@@ -11,35 +11,49 @@ router.post('/setToken', function (req, res) {
     console.log('jira_id', req.body.jira_id);
     let id_firebase = req.body.token;
     let id_jira = req.body.jira_id;
-    UsersModel.findOne({
-        id_jira: id_jira
-    }, (err, user_r) => {
-        if (!user_r) {
-            let n_user = new UsersModel({
-                name: id_jira,
-                id_jira: id_jira,
-                id_firebase: id_firebase,
-            });
-            n_user.save((result) => {
-                if (result) {
-                    console.log('Có lỗi khi lưu user', user);
-                    return;
-                }
-                console.log('Thêm user thành công', result);
-            })
-        } else {
-            user_r.id_firebase = id_firebase;
-            if (!user_r.name) user_r.name = id_jira;
-            user_r.save((result) => {
-                if (result) {
-                    console.log('Có lỗi khi cập nhật user', user);
-                    return;
-                }
-                console.log('Cập nhật thành công');
-            })
-        }
-    });
-    res.json({})
+
+    let task = new Promise(((resolve, reject) => {
+        UsersModel.findOne({
+            id_jira: id_jira
+        }, (err, user_r) => {
+            if (!user_r) {
+                let n_user = new UsersModel({
+                    name: id_jira,
+                    id_jira: id_jira,
+                    id_firebase: id_firebase,
+                });
+                n_user.save((err) => {
+                    if (err) {
+                        resolve({
+                            message: `Có lỗi khi lưu user: ${id_jira}`
+                        });
+                        return;
+                    }
+                    resolve({
+                        message: `Thêm user thành công: ${id_jira}`
+                    });
+                })
+            } else {
+                user_r.id_firebase = id_firebase;
+                if (!user_r.name) user_r.name = id_jira;
+                user_r.save((err) => {
+                    if (err) {
+                        resolve({
+                            message: `Có lỗi khi cập nhật user: ${id_jira}`
+                        });
+                        return;
+                    }
+                    resolve({
+                        message: `Cập nhật thành công: ${id_jira}`
+                    });
+                })
+            }
+        });
+    }));
+
+    task.then(result =>{
+        res.json(result)
+    })
 });
 
 router.get('/*', function (req, res) {
