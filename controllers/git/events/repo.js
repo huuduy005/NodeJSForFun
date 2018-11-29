549170926863ea5935dc321be2816cb4b6ef5519
duydnh <duydnh@younetgroup.c>
2018-11-29 17:54:04 +0700

    craft - refactor - eventKey repo:*

--- controllers/git/events/repo.js
+++ controllers/git/events/repo.js
@@ -1,5 +1,9 @@
 const {getByIdJIRA} = require('../../../services/users');
 const {getChangesCommit, getDataCommit} = require('../../../services/bitbucket/api');
+const {sendTextToUser} = require('../../../services/skype');
+const {git_template} = require('../template');
+
+require('../../../mongoose')();
 
 function handleRepo(key, payload) {
   switch (key) {
@@ -13,64 +17,97 @@ function handleRepo(key, payload) {
 
 module.exports = handleRepo;
 
-
 async function pushEvent(payload) {
   console.log('PushEvent');
+
+  let users = ['duy.doan'];
 
   let {repository, changes} = payload;
   let project = repository.project;
 
-  let commits = await Promise.all(changes.map(({toHash}) => getDataCommit({
-    commitId: toHash,
-    projectName: project.key,
-    reposSlug: repository.slug
-  })));
+  let commits = await Promise.all(
+    changes.map(({fromHash, toHash}) =>
+      getMoreDataCommit({
+        commitId: toHash,
+        projectName: project.key,
+        reposSlug: repository.slug,
+        fromHash
+      })
+    )
+  );
 
-  console.log(commits);
+  for (let item of commits) {
+    let changesCommit = item.changes;
 
+    let fileChanges = {};
+
+    for (let aChange of changesCommit.values) {
+      let path = aChange.path;
+      if (!fileChanges[path.parent]) fileChanges[path.parent] = [];
+      fileChanges[path.parent].push({
+        name: path.name,
+        link: aChange.links.self[0].href,
+        type: aChange.type
+      });
+    }
+    item.fileChanges = fileChanges;
+  }
+
+  payload.changesData = commits;
+  let actionText = git_template(payload);
+  let ids_skype = await getByIdJIRA(users);
+  ids_skype = ids_skype.map(user => user.id_skype);
+
+  ids_skype.map(id => sendTextToUser(actionText, id));
+
+  console.log(actionText);
+}
+
+function getMoreDataCommit(obj) {
+  return Promise.all([getDataCommit(obj), getChangesCommit(obj)]).then(([data, changes]) => ({data, changes}));
 }
 
 let payload = {
-  'eventKey': 'repo:refs_changed',
-  'date': '2018-11-29T16:16:32+0700',
-  'actor': {
-    'name': 'dinh.tran@vexere.com',
-    'emailAddress': 'dinh.tran@vexere.com',
-    'id': 507,
-    'displayName': 'Trần Văn Định',
-    'active': true,
-    'slug': 'dinh.tran_vexere.com',
-    'type': 'NORMAL'
+  eventKey: 'repo:refs_changed',
+  date: '2018-11-29T16:16:32+0700',
+  actor: {
+    name: 'dinh.tran@vexere.com',
+    emailAddress: 'dinh.tran@vexere.com',
+    id: 507,
+    displayName: 'Trần Văn Định',
+    active: true,
+    slug: 'dinh.tran_vexere.com',
+    type: 'NORMAL'
   },
-  'repository': {
-    'slug': 'vxrapi',
-    'id': 13,
-    'name': 'VXRAPI',
-    'scmId': 'git',
-    'state': 'AVAILABLE',
-    'statusMessage': 'Available',
-    'forkable': true,
-    'project': {
-      'key': 'API',
-      'id': 2,
-      'name': 'API',
-      'description': 'VeXeRe API',
-      'public': false,
-      'type': 'NORMAL'
+  repository: {
+    slug: 'vxrapi',
+    id: 13,
+    name: 'VXRAPI',
+    scmId: 'git',
+    state: 'AVAILABLE',
+    statusMessage: 'Available',
+    forkable: true,
+    project: {
+      key: 'API',
+      id: 2,
+      name: 'API',
+      description: 'VeXeRe API',
+      public: false,
+      type: 'NORMAL'
     },
-    'public': false
+    public: false
   },
-  'changes': [
+  changes: [
     {
-      'ref': {
-        'id': 'refs/heads/sandbox',
-        'displayId': 'sandbox',
-        'type': 'BRANCH'
+      ref: {
+        id: 'refs/heads/sandbox',
+        displayId: 'sandbox',
+        type: 'BRANCH'
       },
-      'refId': 'refs/heads/sandbox',
-      'fromHash': '112139bca5253fd7f2bced53a7eae2410d07a672',
-      'toHash': '8ccd9c452312421239f4d30682564bbeccdf6e11',
-      'type': 'UPDATE'
+      refId: 'refs/heads/sandbox',
+      fromHash: '112139bca5253fd7f2bced53a7eae2410d07a672',
+      toHash: '8ccd9c452312421239f4d30682564bbeccdf6e11',
+      type: 'UPDATE'
     }
   ]
 };
