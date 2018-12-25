7bc72159f409851251f6a10d9a97f822d1c3b20f
duydnh <duydnh@younetgroup.com>
2018-12-25 13:57:11 +0700

    fix

--- controllers/skype/index.js
+++ controllers/skype/index.js
@@ -166,7 +166,7 @@ async function sendTextToSkype(text, users) {
         if (!id_skype) return;
         let _token = await token.getToken();
 
-        let _text = id_jira !== 'duy.doan' ? tranformText(text) : text;
+        let _text = _.includes(['default', 'duy.doan'], id_jira) ? tranformText(text) : text;
         let options = genContentToRequest(id_skype, id_skype, _token, _text);
         request(options, function (error, response, body) {
             if (error) console.log('Skype - Lỗi gửi cho user: ', id_skype);
--- controllers/skype/utils/users.js
+++ controllers/skype/utils/users.js
@@ -4,13 +4,15 @@ const defaultChanel = '19:fe672b51b1b24dc084c0bcb40757b846@thread.skype';
 const chanel_id = '19:1ba78d76f663407b96b46c72be8da946@thread.skype';
 const chanel_id_2 = '19:fe672b51b1b24dc084c0bcb40757b846@thread.skype';
 
+const default_chanel = {id_skype: defaultChanel, id_jira: 'default'};
+
 async function getListToSend(ids) {
-    if (ids && ids.length === 0) return [{id_skype: defaultChanel}];
+    if (ids && ids.length === 0) return [default_chanel];
 
     let list_id = ids.map((id) => ({'id_jira': id}));
     try {
         let users = await Users.find({$or: list_id});
-        if (!users || (users && users.length === 0)) return [{id_skype: defaultChanel}];
+        if (!users || (users && users.length === 0)) return [default_chanel];
 
         let ids_skype = users.map(user => ({
             id_jira: user.id_jira,
@@ -19,11 +21,11 @@ async function getListToSend(ids) {
         }));
 
         //TODO: Mặc đinh thêm channel Noti vào danh sách gửi
-        ids_skype.push({id_skype: defaultChanel});
+        ids_skype.push(default_chanel);
         return ids_skype;
     } catch (e) {
         console.error('Fail get users', e);
-        return [{id_skype: defaultChanel}];
+        return [default_chanel];
     }
 }
 
