c0c4cad6e483d4e3e4899a09566e6d675a955619
huuduy <huuduy.lk@gmail.com>
2019-02-24 22:45:16 +0700

    update script tiki

--- scripts/tiki/index.js
+++ scripts/tiki/index.js
@@ -6,15 +6,18 @@ const sendSkype = require('./skype');
 const sendViber = require('./viber');
 require('../../mongoose')();
 
+const list_pid = [4533169, 4538569]; //XS, XS Max
+
 async function main() {
-    let pid = 4533169;
-    let data = await getInfo(pid);
-    console.log(data);
-    let products = _.get(data, 'configurable_products');
-    let productRenders = await Promise.all(products.map(({id: spid}) => ajaxProductRender(pid, spid)));
-    console.log(products);
-    let objData = {data, products, productRenders};
-    await Promise.all([sendSkype(objData), sendViber(objData)]);
+    list_pid.forEach(async pid => {
+        let data = await getInfo(pid);
+        console.log(data);
+        let products = _.get(data, 'configurable_products');
+        let productRenders = await Promise.all(products.map(({id: spid}) => ajaxProductRender(pid, spid)));
+        console.log(products);
+        let objData = {data, products, productRenders};
+        await Promise.all([sendSkype(objData), sendViber(objData)]);
+    });
 }
 
 const CRON_TIME = '0 0,6-23/2 * * *';
